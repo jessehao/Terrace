cf0871ec446b2dbd67505113a8e29a83d4f916d3
Jesse Hao <jeasygates@hotmail.com>
2019-02-21 10:52:05 +0800

    Initial Commit

---
+++ Terrace/Terrace.h
@@ -0,0 +1,19 @@
+//
+//  Terrace.h
+//  Terrace
+//
+//  Created by Jesse Hao on 2019/2/21.
+//  Copyright © 2019 Snoware. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Terrace.
+FOUNDATION_EXPORT double TerraceVersionNumber;
+
+//! Project version string for Terrace.
+FOUNDATION_EXPORT const unsigned char TerraceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Terrace/PublicHeader.h>
+
+
---
+++ TerraceTests/TerraceTests.swift
@@ -0,0 +1,34 @@
+//
+//  TerraceTests.swift
+//  TerraceTests
+//
+//  Created by Jesse Hao on 2019/2/21.
+//  Copyright © 2019 Snoware. All rights reserved.
+//
+
+import XCTest
+@testable import Terrace
+
+class TerraceTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
